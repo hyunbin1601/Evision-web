64a3da2bd1acdf29c02c40183c8404453964964b
지선 <114008429+wltjs1027@users.noreply.github.com>
2024-02-11 17:53:19 +0900

    fit: edit AttendanceRepository

--- back-end/spring/src/main/java/webpage/spring/repository/AttendanceRepository.java
+++ back-end/spring/src/main/java/webpage/spring/repository/AttendanceRepository.java
@@ -1,10 +1,1 @@
-package webpage.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import webpage.spring.domain.AttendanceMember;
-
-import java.util.List;
-
-public interface AttendanceRepository extends JpaRepository<AttendanceMember, Long> {
-    List<AttendanceMember> findAllById(Long id);
-}
+삭제하기
